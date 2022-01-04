4d5ba25de072d5f63b47e2b5270b1c2949f1ccbb
brlaney42 <brlaney@outlook.com>
2022-01-04 12:11:45 -0600

    Successfull talwind addition

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import '../tailwind.css'
+  import Header from '../components/header.svelte'
+</script>
+
+<Header />
+<div class='w-full min-h-screen bg-slate-800 text-slate-50 px-10 md:px-14 lg:px-32 py-14'>
+  <slot></slot>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,15 @@
 <script>
   import {goto} from '$app/navigation';
+
   var searched = '';
   const search = () => {
     goto(`${searched}`)
   }
-
 </script>
 
 <section>
   <div>
-    <div>
-      Search an artist
-    </div>
+    <div>Search an artist</div>
     <input type='text' bind:value={searched} />
     <button on:click={search}>Search</button>
   </div>
